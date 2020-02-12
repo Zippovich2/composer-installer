6fb47a6221f6bf7fcd1874d3b180158f660989e5
Roman Skoropadsky <roman.skoropadsky@idealscorp.com>
2020-02-12 11:22:06 +0200

    init

---
+++ src/Installer.php
@@ -0,0 +1,160 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the "Composer Installer" composer plugin.
+ *
+ * (c) Skoropadskyi Roman <zipo.ckorop@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Zippovich2\ComposerInstaller;
+
+use Composer\Composer;
+use Composer\EventDispatcher\EventSubscriberInterface;
+use Composer\IO\IOInterface;
+use Composer\Plugin\PluginEvents;
+use Composer\Plugin\PluginInterface;
+use Composer\Plugin\PreFileDownloadEvent;
+use Symfony\Component\Dotenv\Dotenv;
+
+/**
+ * @author Skoropadskyi Roman <zipo.ckorop@gmail.com>
+ */
+final class Installer implements PluginInterface, EventSubscriberInterface
+{
+    /**
+     * @var Composer
+     */
+    private $composer;
+
+    /**
+     * @var IOInterface
+     */
+    private $io;
+
+    /**
+     * @var Dotenv
+     */
+    private $dotenv;
+
+    /**
+     * @var string
+     */
+    private static $dir;
+
+    public function __construct()
+    {
+        $this->dotenv = new Dotenv();
+        self::$dir = \getcwd();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function activate(Composer $composer, IOInterface $io): void
+    {
+        $this->composer = $composer;
+        $this->io = $io;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public static function getSubscribedEvents()
+    {
+        return [
+            PluginEvents::PRE_FILE_DOWNLOAD => 'onPreFileDownload',
+        ];
+    }
+
+    /**
+     * Triggered before packages file start loading and change it urls replacing variables.
+     *
+     * @throws \Exception when url contain variable which is not in .env* files or not in environment variables
+     */
+    public function onPreFileDownload(PreFileDownloadEvent $event): void
+    {
+        $url = $event->getProcessedUrl();
+        $placeholders = $this->getPlaceholders($url);
+
+        if (\count($placeholders) > 0) {
+            $envVars = $this->parseEnvVars();
+
+            foreach ($placeholders as $placeholder) {
+                $value = isset($envVars[$placeholder]) ? $envVars[$placeholder] : getenv($placeholder);
+
+                if (empty($value)) {
+                    throw new \Exception(sprintf('Missing environment variable "%s".', $placeholder));
+                }
+                $url = \str_replace('{%' . $placeholder . '%}', $envVars[$placeholder], $url);
+            }
+
+            $remoteFilesystem = $event->getRemoteFilesystem();
+
+            $event->setRemoteFilesystem(
+                new RemoteFilesystem(
+                    $url,
+                    $this->io,
+                    $this->composer->getConfig(),
+                    $remoteFilesystem->getOptions(),
+                    $remoteFilesystem->isTlsDisabled()
+                )
+            );
+        }
+    }
+
+    /**
+     * Parse variables from package url.
+     */
+    public function getPlaceholders(?string $url): array
+    {
+        if (empty($url)) {
+            return [];
+        }
+
+        \preg_match_all('/{%([A-Z0-9_]+)%}/', $url, $matches);
+
+        return \array_unique($matches[1]);
+    }
+
+    /**
+     * Parsing all .env* files and merge theme in order: .env, .env.local, .env.%APP_ENV%, .env.%APP_ENV%.local.
+     */
+    public function parseEnvVars(): array
+    {
+        $envVars = $this->parseEnvFile('.env');
+        $envVars = $this->parseEnvFile('.env.local', $envVars);
+
+        if (isset($envVars['APP_ENV'])) {
+            $envVars = $this->parseEnvFile(\sprintf('.env.%s', $envVars['APP_ENV']), $envVars);
+            $envVars = $this->parseEnvFile(\sprintf('.env.%s.local', $envVars['APP_ENV']), $envVars);
+        }
+
+        return $envVars;
+    }
+
+    /**
+     * Parse .evn file and return it's values as array.
+     *
+     * @param string $file path to the .env* file
+     * @param array  $existedVars values to be merged
+     *
+     * @return array return parsed values which merged with provided $data values or $existedVars if file not exists
+     */
+    public function parseEnvFile(string $file, array $existedVars = []): array
+    {
+        $path = self::$dir . '/' . $file;
+
+        if (!\file_exists($path)) {
+            return $existedVars;
+        }
+
+        $data = \file_get_contents($path);
+
+        return array_merge($existedVars, $this->dotenv->parse($data));
+    }
+}
---
+++ src/RemoteFileSystem.php
@@ -0,0 +1,59 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the "Composer Installer" composer plugin.
+ *
+ * (c) Skoropadskyi Roman <zipo.ckorop@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Zippovich2\ComposerInstaller;
+
+use Composer\Config;
+use Composer\IO\IOInterface;
+use Composer\Util\RemoteFilesystem as ComposerRemoteFilesystem;
+
+/**
+ * @author Skoropadskyi Roman <zipo.ckorop@gmail.com>
+ */
+final class RemoteFileSystem extends ComposerRemoteFilesystem
+{
+    /**
+     * @var string
+     */
+    protected $fileUrl;
+
+    public function __construct(
+        string $fileUrl,
+        IOInterface $io,
+        Config $config = null,
+        array $options = [],
+        $disableTls = false
+    ) {
+        $this->fileUrl = $fileUrl;
+        parent::__construct($io, $config, $options, $disableTls);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function copy(
+        $originUrl,
+        $fileUrl,
+        $fileName,
+        $progress = true,
+        $options = []
+    ) {
+        return parent::copy(
+            $originUrl,
+            $this->fileUrl,
+            $fileName,
+            $progress,
+            $options
+        );
+    }
+}
